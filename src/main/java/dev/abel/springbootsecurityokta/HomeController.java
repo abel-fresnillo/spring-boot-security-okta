73579f3501ef88a20c646f288223b2b5571c9fbd
Abel Fresnillo <me@abel.dev>
2019-07-26 15:08:34 -0400

    Cleans up unused import

--- src/main/java/dev/abel/springbootsecurityokta/HomeController.java
+++ src/main/java/dev/abel/springbootsecurityokta/HomeController.java
@@ -5,8 +5,6 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
-
 @RestController
 public class HomeController {
     @GetMapping("/")
